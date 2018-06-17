db6654b61a139db3034a092897ea9c9cb7df2345
olih <olih@users.noreply.github.com>
2018-06-17 19:45:00 +0100

    wip

--- tests/WidgetTestData.elm
+++ tests/WidgetTestData.elm
@@ -20,7 +20,11 @@ import Bubblegum.Tag.Vocabulary exposing (..)
 
 
 type TestMsg
-    = OnInputContent String
+    = OnSearchInputContent String
+    | OnToggleDropbox
+    | OnAddTag String
+    | OnDeleteTag String
+
 
 biggerThanSmall : Int
 biggerThanSmall = 50
@@ -33,7 +37,10 @@ biggerThanVeryLarge = 200000
 
 defaultAdapter : Adapter.Model TestMsg
 defaultAdapter =
-    { onInput = OnInputContent
+    { onSearchInput = OnSearchInputContent
+    , onToggleDropbox =  OnToggleDropbox
+    , onAddTag = OnAddTag
+    , onDeleteTag = OnDeleteTag
     }
  
 
@@ -53,7 +60,6 @@ defaultSettings = {
 defaultState: StateEntity.Model
 defaultState = {
     attributes = [
-        attr ui_content ipsum
     ]
  }          
       
@@ -81,199 +87,6 @@ findComponent selectors html=
 findWarningDiv: Html.Html TestMsg -> Expectation 
 findWarningDiv html = 
     html |> Query.fromHtml |> Query.findAll [ Selector.class "warning" ] |> Query.count (Expect.atLeast 1)
-
--- The minimum number of characters needed for successful content
-withSettingsSuccessMinimumChars: Int -> SettingsEntity.Model
-withSettingsSuccessMinimumChars value = {
-    attributes = [
-        attr ui_successMinimumChars (value |> toString)
-        , attr ui_successMaximumChars (value + 100 |> toString)
-    ]
- }
-
-fuzzySuccessMinimumChars : Fuzzer Int
-fuzzySuccessMinimumChars=intRange 1 10000  
-
-fuzzyNotSuccessMinimumChars : Fuzzer Int
-fuzzyNotSuccessMinimumChars= Fuzz.oneOf [
-        intRange -70000 -1
-        , intRange 70000 1000000
-    ]
-
-selectorsSuccessMinimumChars : List Selector
-selectorsSuccessMinimumChars = [ Selector.tag "progress" ]
-
--- The maximum number of characters needed for successful content
-withSettingsSuccessMaximumChars: Int -> SettingsEntity.Model
-withSettingsSuccessMaximumChars value = {
-    attributes = [
-        attr ui_successMinimumChars (value |> toString)
-        , attr ui_successMaximumChars (value + 100 |> toString)
-    ]
- }
-
-fuzzySuccessMaximumChars : Fuzzer Int
-fuzzySuccessMaximumChars=intRange 1 10000  
-
-fuzzyNotSuccessMaximumChars : Fuzzer Int
-fuzzyNotSuccessMaximumChars= Fuzz.oneOf [
-        intRange -70000 -1
-        , intRange 70000 1000000
-    ]
-
-selectorsSuccessMaximumChars : List Selector
-selectorsSuccessMaximumChars = [ Selector.tag "progress" ]
-
--- Warning when under the minimum number of characters
-withSettingsDangerMinimumChars: Int -> SettingsEntity.Model
-withSettingsDangerMinimumChars value = {
-    attributes = [
-        attr ui_dangerMinimumChars (value |> toString)
-        , attr ui_dangerMaximumChars (value + 50 |> toString)
-    ]
- }
-
-fuzzyDangerMinimumChars : Fuzzer Int
-fuzzyDangerMinimumChars=intRange 1 200  
-
-fuzzyNotDangerMinimumChars : Fuzzer Int
-fuzzyNotDangerMinimumChars= Fuzz.oneOf [
-        intRange -70000 -1
-        , intRange 70000 1000000
-    ]
-
-selectorsDangerMinimumChars : List Selector
-selectorsDangerMinimumChars = [ Selector.classes ["tag", "is-danger"], 
-    Selector.attribute (Attributes.title "número de caracteres") ]
-
--- Warning when over the maximum number of characters
-withSettingsDangerMaximumChars: Int -> SettingsEntity.Model
-withSettingsDangerMaximumChars value = {
-    attributes = [
-        attr ui_dangerMinimumChars (value |> toString)
-        , attr ui_dangerMaximumChars (value + 100 |> toString)
-    ]
- }
-
-fuzzyDangerMaximumChars : Fuzzer Int
-fuzzyDangerMaximumChars=intRange 1 200  
-
-fuzzyNotDangerMaximumChars : Fuzzer Int
-fuzzyNotDangerMaximumChars= Fuzz.oneOf [
-        intRange -70000 -1
-        , intRange 70000 1000000
-    ]
-
-selectorsDangerMaximumChars : List Selector
-selectorsDangerMaximumChars = [ Selector.classes ["tag", "is-danger"],
- Selector.attribute (Attributes.title "número de caracteres") ]
-
--- The minimum number of words needed for successful content
-withSettingsSuccessMinimumWords: Int -> SettingsEntity.Model
-withSettingsSuccessMinimumWords value = {
-    attributes = [
-        attr ui_successMinimumWords (value |> toString)
-        , attr ui_successMaximumWords (value + 100 |> toString)
-    ]
- }
-
-fuzzySuccessMinimumWords : Fuzzer Int
-fuzzySuccessMinimumWords=intRange 1 10000  
-
-fuzzyNotSuccessMinimumWords : Fuzzer Int
-fuzzyNotSuccessMinimumWords= Fuzz.oneOf [
-        intRange -70000 -1
-        , intRange 70000 1000000
-    ]
-
-selectorsSuccessMinimumWords : List Selector
-selectorsSuccessMinimumWords = [ Selector.tag "progress" ]
-
--- The maximum number of words needed for successful content
-withSettingsSuccessMaximumWords: Int -> SettingsEntity.Model
-withSettingsSuccessMaximumWords value = {
-    attributes = [
-        attr ui_successMinimumWords (value |> toString)
-        , attr ui_successMaximumWords (value + 100 |> toString)
-    ]
- }
-
-fuzzySuccessMaximumWords : Fuzzer Int
-fuzzySuccessMaximumWords=intRange 1 10000  
-
-fuzzyNotSuccessMaximumWords : Fuzzer Int
-fuzzyNotSuccessMaximumWords= Fuzz.oneOf [
-        intRange -70000 -1
-        , intRange 70000 1000000
-    ]
-
-selectorsSuccessMaximumWords : List Selector
-selectorsSuccessMaximumWords = [ Selector.tag "progress" ]
-
--- Warning when under the minimum number of words
-withSettingsDangerMinimumWords: Int -> SettingsEntity.Model
-withSettingsDangerMinimumWords value = {
-    attributes = [
-        attr ui_dangerMinimumWords (value |> toString)
-        , attr ui_dangerMaximumWords (value + 20 |> toString)
-    ]
- }
-
-fuzzyDangerMinimumWords : Fuzzer Int
-fuzzyDangerMinimumWords=intRange 1 10  
-
-fuzzyNotDangerMinimumWords : Fuzzer Int
-fuzzyNotDangerMinimumWords= Fuzz.oneOf [
-        intRange -70000 -1
-        , intRange 70000 1000000
-    ]
-
-selectorsDangerMinimumWords : List Selector
-selectorsDangerMinimumWords = [ Selector.classes ["tag", "is-danger"],
-    Selector.attribute (Attributes.title "número de palabras") ]
-
--- Warning when over the maximum number of words
-withSettingsDangerMaximumWords: Int -> SettingsEntity.Model
-withSettingsDangerMaximumWords value = {
-    attributes = [
-        attr ui_dangerMinimumWords (value |> toString)
-        , attr ui_dangerMaximumWords (value + 20 |> toString)
-    ]
- }
-
-fuzzyDangerMaximumWords : Fuzzer Int
-fuzzyDangerMaximumWords=intRange 1 30  
-
-fuzzyNotDangerMaximumWords : Fuzzer Int
-fuzzyNotDangerMaximumWords= Fuzz.oneOf [
-        intRange -70000 -1
-        , intRange 70000 1000000
-    ]
-
-selectorsDangerMaximumWords : List Selector
-selectorsDangerMaximumWords = [ Selector.classes ["tag", "is-danger"],
- Selector.attribute (Attributes.title "número de palabras") ]
-
--- Short hint describing the expected content
-withSettingsPlaceholder: Int -> SettingsEntity.Model
-withSettingsPlaceholder value = {
-    attributes = [
-        attr ui_placeholder (createString value)
-    ]
- }
-
--- a bit of a hack. Would be nice if we would not have to specify the exact value for placeholder.
-fuzzyPlaceholder : Fuzzer Int
-fuzzyPlaceholder=intRange 50 50 
-
-fuzzyNotPlaceholder : Fuzzer Int
-fuzzyNotPlaceholder= intRange 300 500 
-
-selectorsPlaceholder : List Selector
-selectorsPlaceholder = [ Selector.class "bubblegum-tag__input", Selector.attribute (attribute "placeholder" (createString 50))]
-
-selectorsNotPlaceholder : List Selector
-selectorsNotPlaceholder = [ Selector.class "bubblegum-tag__input", Selector.attribute (attribute "data-bubblegum-warn" "unsatisfied-constraint:within-string-chars-range:(1,128)")]
 
 -- Label related to the field
 withSettingsLabel: Int -> SettingsEntity.Model
@@ -308,23 +121,6 @@ fuzzyNotHelp= intRange 300 400
 
 selectorsHelp : List Selector
 selectorsHelp = [ Selector.classes ["help", "is-info"] ]
-
--- Tag used to describe the field
-withSettingsTag: Int -> SettingsEntity.Model
-withSettingsTag value = {
-    attributes = [
-        attr ui_tag (createString value)
-    ]
- }
-
-fuzzyTag : Fuzzer Int
-fuzzyTag=intRange 1 30
-
-fuzzyNotTag : Fuzzer Int
-fuzzyNotTag= intRange 300 400
-
-selectorsTag : List Selector
-selectorsTag = [ Selector.attribute (Attributes.title "etiqueta de información") ]
 
 -- Language used by the user
 
@@ -444,101 +240,6 @@ fuzzyNotDangerHelp= intRange 300 400
 
 selectorsDangerHelp : List Selector
 selectorsDangerHelp = [ Selector.classes ["help", "is-danger"] ]
-
--- The content of the field
-withStateContent: Int -> SettingsEntity.Model
-withStateContent value = {
-    attributes = [
-        attr ui_content (createString value)
-    ]
- }
-
-fuzzyContent : Fuzzer Int
-fuzzyContent= constant 1000
-
-fuzzyNotContent : Fuzzer Int
-fuzzyNotContent= intRange biggerThanVeryLarge (10*biggerThanVeryLarge)
-
-selectorsContent : List Selector
-selectorsContent = [ Selector.tag "tag" , Selector.attribute (Attributes.value (createString 1000))]
-
-selectorsNotContent : List Selector
-selectorsNotContent = [Selector.attribute (attribute "data-bubblegum-warn" "unsatisfied-constraint:within-string-chars-range:(0,60000)") ]
-
--- Tag representing a successful facet of the content
-withStateSuccessTag: Int -> SettingsEntity.Model
-withStateSuccessTag value = {
-    attributes = [
-        attr ui_successTag (createString value)
-    ]
- }
-
-fuzzySuccessTag : Fuzzer Int
-fuzzySuccessTag=intRange 1 30
-
-fuzzyNotSuccessTag : Fuzzer Int
-fuzzyNotSuccessTag= intRange 300 400
-
-selectorsSuccessTag : List Selector
-selectorsSuccessTag = [ Selector.classes ["tag", "is-success"],
-    Selector.attribute (Attributes.title "etiqueta que indica el éxito") ]
-
--- Tag representing a warning aspect of the content
-withStateWarningTag: Int -> SettingsEntity.Model
-withStateWarningTag value = {
-    attributes = [
-        attr ui_warningTag (createString value)
-    ]
- }
-
-fuzzyWarningTag : Fuzzer Int
-fuzzyWarningTag=intRange 1 30
-
-fuzzyNotWarningTag : Fuzzer Int
-fuzzyNotWarningTag= intRange 300 400
-
-selectorsWarningTag : List Selector
-selectorsWarningTag = [ Selector.classes ["tag", "is-warning"],
-    Selector.attribute (Attributes.title "etiqueta que indica advertencia") ]
-
--- Tag representing a dangerous aspect of the content
-withStateDangerTag: Int -> SettingsEntity.Model
-withStateDangerTag value = {
-    attributes = [
-        attr ui_dangerTag (createString value)
-    ]
- }
-
-fuzzyDangerTag : Fuzzer Int
-fuzzyDangerTag=intRange 1 30
-
-fuzzyNotDangerTag : Fuzzer Int
-fuzzyNotDangerTag= intRange 300 400
-
-selectorsDangerTag : List Selector
-selectorsDangerTag = [ Selector.classes ["tag", "is-danger"],
-    Selector.attribute (Attributes.title "etiqueta que indica peligro") ]
-
--- The unique id of the content
-withStateContentId: Int -> SettingsEntity.Model
-withStateContentId value = {
-    attributes = [
-        attr ui_contentId (createString value)
-    ]
- }
-
-fuzzyContentId : Fuzzer Int
-fuzzyContentId=constant 100
-
-fuzzyNotContentId : Fuzzer Int
-fuzzyNotContentId= intRange biggerThanMedium (4*biggerThanMedium)
-
-selectorsContentId : List Selector
-selectorsContentId = [ Selector.tag "tag" , Selector.attribute (Attributes.id (createString 100))]
-
-selectorsNotContentId : List Selector
-selectorsNotContentId = [ Selector.tag "tag" ,
-    Selector.attribute (attribute "data-bubblegum-warn" "unsatisfied-constraint:within-string-chars-range:(1,128)") ]
 
 -- private
 
